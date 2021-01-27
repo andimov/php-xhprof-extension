9fe512819836784803e6d78b187ebe1999785da0
Andrii Dimov <andimov@gmail.com>
2021-01-27 01:34:12 -0600

    PHP 8 support

--- spans.h
+++ spans.h
@@ -1,9 +1,9 @@
 #ifndef TIDEWAYS_SPANS_H
 #define TIDEWAYS_SPANS_H
-long tw_span_create(char *category, size_t category_len TSRMLS_DC);
-void tw_span_annotate(long spanId, zval *annotations TSRMLS_DC);
-void tw_span_annotate_long(long spanId, char *key, long value TSRMLS_DC);
-void tw_span_annotate_string(long spanId, char *key, char *value, int copy TSRMLS_DC);
+long tw_span_create(char *category, size_t category_len);
+void tw_span_annotate(long spanId, zval *annotations);
+void tw_span_annotate_long(long spanId, char *key, long value);
+void tw_span_annotate_string(long spanId, char *key, char *value, int copy);
 
 #ifndef ZVAL_COPY_VALUE
 #define ZVAL_COPY_VALUE(z, v)              \
